1463a90ee1018eea04681c455321e180399b21f5
rasitdeveloper <33697532+rasitdeveloper@users.noreply.github.com>
2022-02-01 03:02:59 +0300

    Update week 2 routes brand

--- Week 2/routes/brand/brand.js
+++ Week 2/routes/brand/brand.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 let brandController = require('../../controllers/brand') // brandController definition that allows us to use the functions in the brandController
 
-router.get('/', brandController.fetchBrand); // if the root directory of the brand is wanted to be reached with get, the fetchBrand function in the brandController will work
+router.get('/', brandController.fetchBrands); // if the root directory of the brand is wanted to be reached with get, the fetchBrand function in the brandController will work
 router.get('/category/:id', brandController.singleFetchCategory);
 router.get('/categories', brandController.fetchCategories);
 router.get('/brand/:name', brandController.singleFetchBrand);
